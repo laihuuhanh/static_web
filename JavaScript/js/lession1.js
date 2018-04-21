907d2d92fb223c6cd59c595438007f4a571a6ae0
laihuuhanh <laihuuhanh1@gmail.com>
2018-04-21 15:42:05 +0700

    a-men

---
+++ JavaScript/js/lession1.js
@@ -0,0 +1,7 @@
+document.getElementById("demo1").innerHTML = "Tài liệu học CSS";
+document.getElementById("demo2").innerHTML = "Tài liệu học MySQL";
+document.getElementById("demo3").innerHTML = "Tài liệu học PHP";
+var a = 10;
+var b = 25;
+var c = a + b;
+alert(c);
