d662e93f584fc103dc29ce0cc0904cabe3c30319
laihuuhanh <laihuuhanh1@gmail.com>
2018-04-23 00:39:27 +0700

    a-amen

--- JavaScript/js/lession2.js
+++ JavaScript/js/lession2.js
@@ -1,1 +1,7 @@
-lession2.js
+document.getElementById("demo1").innerHTML = "Tài liệu học CSS";
+document.getElementById("demo2").innerHTML = "Tài liệu học MySQL";
+document.getElementById("demo3").innerHTML = "Tài liệu học PHP";
+var a = 10;
+var b = 25;
+var c = a + b;
+alert(c);
