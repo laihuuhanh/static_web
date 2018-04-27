93185312dde05731bca2833c7b93b4031b0285ee
laihuuhanh <laihuuhanh1@gmail.com>
2018-04-27 11:10:27 +0700

    amen

---
+++ JavaScript/js/lession6.js
@@ -0,0 +1,70 @@
+function clickMe() {
+    alert('click me!!!');
+}
+
+function doubleClickMe(){
+    alert('double click me!!!');
+}
+
+function onMouseEnterMe() {
+    alert('mouse enter me!');
+}
+
+function onMouseLeaveMe() {
+    alert('mouse leave me!');
+}
+
+function onKeyDownMe () {
+    alert('on key down me!');
+}
+
+function onKeyUpMe () {
+    alert('on key up me!');
+}
+
+function copyMe() {
+    alert('copy me');
+}
+
+function cutMe() {
+    alert('cut me');
+}
+
+function pasteMe(){
+    alert('paste me!');
+}
+
+function changeMe() {
+	value="html"
+    value="css"
+    value="javascript"
+    value="mysql"
+    value="php"
+    alert('Bạn vừa thay đổi giá trị của phần tử này');
+}
+
+//bai tap
+function phepTinh(){
+var a = 100;var b = 50;var result = (a + b)*2;
+alert('Kết quả của biểu thức là: ' + result) 
+}
+
+//
+function hello(){
+            var name = "lại Hữu Hạnh";
+            var year = 1998;
+            var str = "Tôi tên " + name + " sinh năm " + year;
+            document.getElementById("chaoXin").innerHTML = str;
+}
+//
+// function onKeyDownLear(){
+// 	var color :#pink;
+// 	document.write('Hello')
+// }
+
+// function onCut1() {
+// 	cols="50" rows="15" 
+// 	oncopy="alert('Bạn vừa sao chép nội dung của phần tử này')" 
+// 	oncut="alert('Bạn vừa cắt nội dung của phần tử này')" 
+// 	onpaste="alert('Bạn vừa dán nội dung vào phần tử này')"
+// }
